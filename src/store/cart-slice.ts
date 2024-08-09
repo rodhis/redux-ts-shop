de7f016b191d7bbe93eca13acd5a46e86553242d
Rodhis <rodrigonoliva@gmail.com>
2024-08-09 17:30:43 -0300

    added add and remove item from cart functions

--- src/store/cart-slice.ts
+++ src/store/cart-slice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 
 type CartItem = {
@@ -20,9 +20,23 @@ export const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        addToCart(state, action) {
-            
+        addToCart(state, action:PayloadAction<{id: string, title: string, price: number}>) { //same as useReducer
+            const itemIndex = state.items.findIndex(item => item.id === action.payload.id);
+            if (itemIndex >= 1) {
+                state.items[itemIndex].quantity++;
+            } else {
+                state.items.push({
+                    ...action.payload, quantity: 1
+                })
+            }
         },
-        removeFromCart() {}
+        removeFromCart(state, action:PayloadAction<string>) {
+            const itemIndex = state.items.findIndex(item => item.id === action.payload);
+            if (itemIndex === 1) {
+                state.items.splice(itemIndex, 1);
+            } else {
+                state.items[itemIndex].quantity--;
+            }
+        }
     }
 })
