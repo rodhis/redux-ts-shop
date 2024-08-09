149ef09bbb3eb67a5e9270b8fddee357c7dda5dd
Rodhis <rodrigonoliva@gmail.com>
2024-08-09 17:35:51 -0300

    finished cart slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import Header from './components/Header.tsx';
-import Shop from './components/Shop.tsx';
-import Product from './components/Product.tsx';
-import { DUMMY_PRODUCTS } from './dummy-products.ts';
+import { Provider } from "react-redux"
+
+import Header from "./components/Header.tsx"
+import Shop from "./components/Shop.tsx"
+import Product from "./components/Product.tsx"
+import { DUMMY_PRODUCTS } from "./dummy-products.ts"
+import {store} from "./store/store.ts"
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Shop>
-        {DUMMY_PRODUCTS.map((product) => (
-          <li key={product.id}>
-            <Product {...product} />
-          </li>
-        ))}
-      </Shop>
-    </>
-  );
+    return (
+        <Provider store={store}>
+            <Header />
+            <Shop>
+                {DUMMY_PRODUCTS.map((product) => (
+                    <li key={product.id}>
+                        <Product {...product} />
+                    </li>
+                ))}
+            </Shop>
+        </Provider>
+    )
 }
 
-export default App;
+export default App
--- src/store/cart-slice.ts
+++ src/store/cart-slice.ts
@@ -40,3 +40,5 @@ export const cartSlice = createSlice({
         }
     }
 })
+
+export const { addToCart, removeFromCart } = cartSlice.actions
