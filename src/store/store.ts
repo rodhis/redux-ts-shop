ccca79f99e93c18f285436b4b2ebdbe18ed83518
Rodhis <rodrigonoliva@gmail.com>
2024-08-13 09:12:37 -0300

    created more custom types for my custom hooks

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { cartSlice } from "./cart-slice";
+import { cartSlice } from "./cart-slice.ts";
 
 
 export const store = configureStore({
     reducer: {
         cart: cartSlice.reducer
     }})
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
